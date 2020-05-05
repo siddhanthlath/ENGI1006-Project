3cdc6bb1ed3811bf9854ee84f7c7e74e7fc049fa
siddhanthlath <64680341+siddhanthlath@users.noreply.github.com>
2020-05-06 00:47:07 +0530

    Second Commit, made homepage and added hyperlinks to other pages

--- app.py
+++ app.py
@@ -17,11 +17,19 @@ app = Flask(__name__)
 #static route
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    return render_template("main_page.html")
 
-@app.route("/1006")
+@app.route("/page1")
+def page1():
+    return render_template("page1.html")
+
+@app.route("/page2")
 def page2():
-    return render_template("1006.html")
+    return render_template("page2.html")
+
+@app.route("/page3")
+def page3():
+    return render_template("page3.html")
 
 #start the server
 if __name__ == "__main__":
